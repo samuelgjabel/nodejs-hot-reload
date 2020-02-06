12f2dc9db169c67cc800ff6262cb4a5fdd50d679
samuel.gjabel <samuel.gjabel@swanmobile.sk>
2020-02-06 22:56:51 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Node.js hot reload
+
+Fast node.js hot-reload library. 🚀🚀
+
+- library is little wrapper around [parcel](https://en.parceljs.org/) library for enabling hot-reload for `nodejs` projects.
+
+### Install
+
+`npm i nodejs-hot-reload`
+or
+`yarn add nodejs-hot-reload`
+
+### Use
+
+1. add this `script` to package.json
+   - for js
+
+```json
+  "scripts": {"watch":"npx nodejs-hot-reload entry=./src/app.js outDir=./build"},
+```
+
+- or for ts
+
+```json
+  "scripts": {"watch":"npx nodejs-hot-reload entry=./src/app.ts outDir=./build"},
+```
+
+2. run script `npm run watch` or `yarn watch`
+
+3. That's it. 🤗
+
+- You can use it as global module as well. But I recommend to use it locally.
+  `npm i -g nodejs-hot-reload`.
+
+### Available flags
+
+| Name              | Use                 | Description                                                                                | default        |
+| ----------------- | ------------------- | ------------------------------------------------------------------------------------------ | -------------- |
+| entry             | `entry=./index.js`  | Entry file                                                                                 | ./scr/index.js |
+| inspect           | `inspect`           | enable inspect. [More here.](https://nodejs.org/de/docs/guides/debugging-getting-started/) | false          |
+| outDir            | `outDir=./build`    | output directory for build                                                                 | ./build        |
+| outDir            | `outDir=./build`    | output directory for build                                                                 | ./build        |
+| cacheDir          | `cacheDir=./cache`  | cache direactory                                                                           | /cache         |
+| minify            | `minify`            | if minify                                                                                  | false          |
+| bundleNodeModules | `bundleNodeModules` | if bundle node_modules                                                                     | false          |
+| scopeHoist        | `scopeHoist`        | Turn on experimental scope hoisting/tree shaking flag, for smaller production bundles.     | false          |
+
+- works with typescript / javascript as well
+
+* note this library dont include typchecking for `typescript`. Your IDE do it for you.
---
+++ index.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+const a = process.argv.slice(2);
+const _dir = process.cwd();
+const path = require("path");
+const chalk = require("chalk");
+const Bundler = require("parcel-bundler");
+
+const child_process = require("child_process");
+const fs = require("fs");
+
+const args = {};
+for (let i = 0; i < a.length; i++) {
+  if (a[i].substring(0, 4) === "out=") {
+    args["out"] = a[i].substring(4);
+  } else if (a[i].substring(0, 6) === "entry=") {
+    args["entry"] = a[i].substring(6);
+  } else if (a[i].substring(0, 9) === "cacheDir=") {
+    args["cacheDir"] = a[i].substring(9);
+  } else {
+    args[a[i]] = true;
+  }
+}
+
+let config = {
+  inspect: args.inspect ? true : false,
+  scopeHoist: args.scopeHoist ? true : false,
+  outDir: path.resolve(_dir, args.out ? args.out : "./build"),
+  entry: path.resolve(_dir, args.entry ? args.entry : "./src/index.js"),
+  cacheDir: path.resolve(_dir, args.cacheDir ? args.cacheDir : "./cache"),
+  minify: args.minify ? true : false,
+  cache: args.cache === false ? false : true,
+  bundleNodeModules: args.bundleNodeModules ? true : false
+};
+
+const outSplit = config.entry.split("/");
+let outFile = outSplit[outSplit.length - 1];
+const fileExtensionSplit = outFile ? outFile.split(".") : [".js"];
+const fileExtension = fileExtensionSplit[fileExtensionSplit.length - 1];
+outFile = outFile.replace(`.${fileExtension}`, ".js");
+const outFileFullPath = `${config.outDir}/${outFile}`;
+
+if (fs.existsSync(outFileFullPath)) {
+  fs.unlinkSync(outFileFullPath);
+}
+console.log(`Entry: ${chalk.blue(config.entry)}`);
+
+const options = {
+  scopeHoist: config.scopeHoist,
+  outDir: config.outDir,
+  watch: true,
+  minify: false,
+  sourceMaps: false,
+  target: "node",
+  cache: config.cache,
+  cacheDir: config.cacheDir,
+  bundleNodeModules: config.bundleNodeModules,
+  logLevel: 0
+};
+
+let child;
+const ora = require("ora");
+let spinner = ora("Building...");
+(async () => {
+  const bundler = new Bundler([config.entry], options);
+  bundler.on("buildStart", () => {
+    if (child) {
+      child.kill();
+    }
+    spinner.start();
+  });
+  bundler.on("buildEnd", () => {
+    let commands = [];
+    spinner.stop();
+    if (config.inspect) {
+      commands = ["--inspect", outFileFullPath];
+    } else {
+      commands = [outFileFullPath];
+    }
+
+    child = child_process.spawn("node", commands, {
+      stdio: "inherit",
+      shell: false
+    });
+    child.on("exit", function(e, code) {
+      child.kill();
+    });
+  });
+  await bundler.bundle();
+})();
